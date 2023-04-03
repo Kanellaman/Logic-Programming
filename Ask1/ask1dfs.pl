e11b190ac346d2e669f9b4758bb854a7ff0a920a
Kanellaman <sdi2000064@di.uoa.gr>
2023-04-03 19:44:21 +0300

    Commit

--- Ask1/ask1dfs.pl
+++ Ask1/ask1dfs.pl
@@ -1,6 +1,7 @@
 pancakes_dfs(State1,Operators,States) :-
     length(State1,N),
     create_list(N,L),
+    valid(State1,L),
     reverse(L,RL),
     dfs(State1, [State1], States,[], Operators, RL).
 
@@ -21,12 +22,11 @@ move(State,NewState,S):-
 
 kane(State,NewState,S):-
     em(State,S,ToFlip),
-    append(ToFlip,[S],Flip),
-    reverse(Flip,Flipped),
+    reverse(ToFlip,Flipped),
     em1(State,S,NoFlipped),
     append(Flipped,NoFlipped,NewState).
 
-em([X|Y],X,[]).
+em([X|Y],X,[X]).
 em([X|Y],S,[X|A]):-
     em(Y,S,A).
 
@@ -40,15 +40,13 @@ create_list(N, [N|Rest]) :-
     N1 is N - 1,
     create_list(N1, Rest).
 
-between(L,U,L):-
-    L=<U.
-between(L,U,X):-
-    L<U,
-    L1 is L+1,
-    between(L1,U,X).
-
 are_same([], []).
 are_same([H1|T1], [H1|T2]) :-
   H1 = H1,
   are_same(T1, T2).
 
+valid([],_).
+valid([X|Y],L):-
+    member(X,L),    % Element out of range
+    \+ member(X,Y), % Duplicates
+    valid(Y,L).
--- Ask1/ask1ids.pl
+++ Ask1/ask1ids.pl
@@ -1,6 +1,7 @@
 pancakes_ids(State1,Operators,States) :-
     length(State1,N),
     create_list(N,L),
+    valid(State1,L),
     reverse(L,RL),
     solve_ids(State1,States,Operators,RL,0).
 
@@ -27,18 +28,17 @@ move(State,NewState,S):-
 
 kane(State,NewState,S):-
     em(State,S,ToFlip),
-    append(ToFlip,[S],Flip),
-    reverse(Flip,Flipped),
+    reverse(ToFlip,Flipped),
     em1(State,S,NoFlipped),
     append(Flipped,NoFlipped,NewState).
 
-em([X1|Y1],X1,[]).
-em([X1|Y1],S,[X1|A]):-
-    em(Y1,S,A).
+em([X|Y],X,[X]).
+em([X|Y],S,[X|A]):-
+    em(Y,S,A).
 
-em1([X1|Y1],X1,Y1).
-em1([X1|Y1],S,A):-
-    em1(Y1,S,A).
+em1([X|Y],X,Y).
+em1([X|Y],S,A):-
+    em1(Y,S,A).
 
 create_list(1, [1]).
 create_list(N, [N|Rest]) :-
@@ -46,14 +46,13 @@ create_list(N, [N|Rest]) :-
     N1 is N - 1,
     create_list(N1, Rest).
 
-between(L,U,L):-
-    L=<U.
-between(L,U,X):-
-    L<U,
-    L1 is L+1,
-    between(L1,U,X).
-
 are_same([], []).
 are_same([H1|T1], [H1|T2]) :-
   H1 = H1,
   are_same(T1, T2).
+
+valid([],_).
+valid([X|Y],L):-
+    member(X,L),
+    \+ member(X,Y),
+    valid(Y,L).

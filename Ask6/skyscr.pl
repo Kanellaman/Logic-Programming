e6c6d197cc508b8f759a7b07f57b59e8876e365a
sdi2000064@di.uoa.gr <sdi2000064@di.uoa.gr>
2023-06-02 11:55:40 +0300

    solution for demo puzzle-Not for all

--- Ask6/skyscr.pl
+++ Ask6/skyscr.pl
@@ -1,12 +1,14 @@
 :- compile(skies).
-:- lib(ic).
 :- lib(ic_global).
+:- lib(ic).
+:- import alldifferent/1 from ic.
 
 skyscr(P,Solution):-
     puzzle(P,Dim,L,R,U,D,Board),
-    transpose(Board,TBoard),
-    cross(Dim,Solution),
-    fill(Board,1).
+    Solution = Board,
+    %fill(Board,1),
+    cross(L,R,U,D,Dim,Solution),
+    search(Solution,0,occurence,indomain,complete,[search_optimization(true)]).
 
 transpose([], []).
 transpose([[]|_], []).
@@ -18,20 +20,45 @@ transpose([], [], []).
 transpose([[X|Xs]|RestMatrix], [X|XValues], [Xs|RestRows]) :-
     transpose(RestMatrix, XValues, RestRows).
 
-cross(N,L):-    % Make 2dimensional List to represent board
-    length(L,N),
-    subL(N,L).
-subL(_,[]).
-subL(N,[X|L]):-
-    length(X,N),
+constraints([],[],[],_).
+constraints([L|RestL],[R|RestR],[X|Rest],N):-
+    alldifferent(X),
+    (L=0,R=0->constraints(RestL,RestR,Rest,N);
+    length(List,N),
+    (L=0->reverse(X,Elements),E#=R;Elements=X,E#=L),
+    writeln(Elements),
+    first(Elements,X1,RestX),
+    visible(List,RestX,[X1],Sum),
+    eval(Sum,E),
+    constraints(RestL,RestR,Rest,N)).
+
+first([X|Rest],X,Rest).
+
+visible([],[],_,1).
+visible([Y|RestY],[X|RestX],SoFar,Sum):-
+    Y #= ( X #> max(SoFar) ),
+    append(SoFar,[X],New),
+    visible(RestY,RestX,New,PrevSum),
+    Sum #= PrevSum + Y.
+
+cross(L,R,U,D,Dim,Rows):-    % Make 2dimensional List to represent board
+    subL(Dim,Rows),
+    print2d(Rows),
+    write('\n'),
+    N is Dim - 1,
+    constraints(L,R,Rows,N),
+    transpose(Rows,TRows),
+    constraints(U,D,TRows,N).
+
+subL(N,[]).
+subL(N,[X|Rest]):-
     X #:: 1..N,
-    subL(N,L).
+    subL(N,Rest).
 
 fill([],_).     % Fill the crossword with i,j positions
 fill([Row|Rest],N):-
     fill(Row,N,1),
     New is N + 1,
-    writeln(Row),
     fill(Rest,New).
 
 fill([],_,_).   % Fill each row with i,j position in the crossword

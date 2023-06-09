8d374fd4abed2f9516b91516899e2503c8a24121
Kanellaman <sdi2000064@di.uoa.gr>
2023-06-09 21:57:07 +0300

    No assignment overlaping

--- Ask7/assignment_csp.pl
+++ Ask7/assignment_csp.pl
@@ -1,13 +1,17 @@
 :- compile(activities).
-:- lib(gfd).
-:- lib(gfd_search).
-:- import search/6 from gfd_search.
+:- lib(ic).
 
-assignment_csp(NP,MT,Workers,Flatten):-
-    length(Workers,NP),
+assignment_csp(NP,MT,Assignments,Flatten):-
+    findall(AId, activity(AId, _), AIds),
+    writeln(AIds),
+    length(AIds,N),
+    length(Assignments,N),
+    Assignments #:: 1..NP,
+    overlap(AIds,Assignments),
+    /* length(Workers,NP),
     subL(MT,Workers,NP,Flatten),
-    alldifferent(Flatten),
-    search(Workers,0,most_constrained,indomain,complete,[search_optimization(true)]),!.
+    alldifferent(Flatten), */
+    search(Assignments,0,most_constrained,indomain,complete,[search_optimization(true)]),!.
 
 subL(_,[],_,[]).
 subL(MT,[X|Rest],NP,FLattened):-
@@ -15,3 +19,18 @@ subL(MT,[X|Rest],NP,FLattened):-
     X #:: 1..MT,
     subL(MT,Rest,NP,Flat),
     append(Flat,X,FLattened).
+
+overlap([_|[]],[_|[]]).
+overlap([AId|RestAids],[Var|RestVar]):-
+    % write(AId),write('       '),
+    check(AId,RestAids,Var,RestVar),
+    % write('\n'),
+    overlap(RestAids,RestVar).
+
+check(_,[],_,[]).
+check(AId,[X|RestAids],Var,[XVar|RestVar]):-
+    activity(AId, act(Ab1, Ae1)),
+    activity(X, act(Ab2, Ae2)),
+    ((Ab1 > Ae2; Ae1 < Ab2) -> true;
+    Var #\= XVar/* ,write(X) */),
+    check(AId,RestAids,Var,RestVar).

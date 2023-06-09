8e1f8e76e6cc0ac9e7f0e23d33ef8695b30ad0e6
Kanellaman <sdi2000064@di.uoa.gr>
2023-06-10 02:07:54 +0300

    All constraints are satisfied-Finds all solutions No symmetric solutions rejection yet Refactor

--- Ask7/assignment_csp.pl
+++ Ask7/assignment_csp.pl
@@ -4,23 +4,26 @@
 :- import search/6 from gfd_search.
 
 assignment_csp(NP,MT,ASP,ASA):-
+    /* Data in Lists */
     findall(AId, activity(AId, _), AIds),
     length(AIds,N),
     length(Assignments,N),
     length(Duration,N),
+
+    /* Constraints */
     Assignments #:: 1..NP,
-    overlap(AIds,Assignments,Duration),
+    overlap(AIds,Assignments,Durations),
+    max_time(NP, Durations, 0, MT),
+
     search(Assignments,0,most_constrained,indomain,complete,[search_optimization(true)]),
     results(AIds,Assignments,ASA),
     makeASP(NP,ASP,0,ASA).
 
 overlap([],[],[]).
-overlap([AId|RestAids],[Var|RestVars],[Duration|RestDurations]):-
-    % write(AId),write('       '),
+overlap([AId|RestAids],[Var|RestVars],[Var-Duration|RestDurations]):-
     activity(AId, act(Ab, Ae)),
     Duration is Ae - Ab,
     check(AId,RestAids,Var,RestVars),
-    % write('\n'),
     overlap(RestAids,RestVars,RestDurations).
 
 check(_,[],_,[]).
@@ -28,7 +31,7 @@ check(AId,[X|RestAids],Var,[XVar|RestVars]):-
     activity(AId, act(Ab1, Ae1)),
     activity(X, act(Ab2, Ae2)),
     ((Ab1 > Ae2; Ae1 < Ab2) -> true;
-    Var #\= XVar/* ,write(X) */),
+    Var #\= XVar),
     check(AId,RestAids,Var,RestVars).
 
 results([],[],[]).
@@ -43,3 +46,17 @@ makeASP(NP,[New-AIds|Rest],N,ASA):-
     makeASP(NP,Rest,New,ASA).
 
 match(Worker, AId-Worker, AId).
+
+max_time(NP, _, NP, _).
+max_time(NP, Durations, N, MT):-
+    NP > N,
+    New is N + 1,
+    constraints(Durations,New,Sum),
+    Sum #< MT + 1,
+    writeln(Sum),
+    max_time(NP, Durations, New, MT).
+
+constraints([],_,0).
+constraints([Var-Duration|RestDurations],N,Sum):-
+    constraints(RestDurations,N,S),
+    Sum #= S + (Var #= N)*Duration.

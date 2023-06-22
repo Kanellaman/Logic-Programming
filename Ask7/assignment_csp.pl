cd5e11f0720e7484ac9c61d17fa5d37da9e9d4d9
sdi2000064@di.uoa.gr <sdi2000064@di.uoa.gr>
2023-06-22 12:49:06 +0300

    Refactor

--- Ask7/assignment_csp.pl
+++ Ask7/assignment_csp.pl
@@ -1,65 +1,68 @@
 :- compile(activities).
 :- lib(ic).
 
-assignment_csp(NP,MT,ASP,ASA):-
+assignment_csp(NP, MT, ASP, ASA) :-
     /* Data in Lists */
     findall(AId, activity(AId, _), AIds),
-    length(AIds,N),
-    length(Assignments,N),
-    length(Duration,N),
+    length(AIds, N),
+    length(Assignments, N),
+    length(Durations, N),
 
     /* Constraints */
     Assignments #:: 1..NP,
-    overlap(AIds,Assignments,Durations),
+    overlap(AIds, Assignments, Durations),
     max_time(NP, Durations, 0, MT, [], Sums),
-    first(Assignments,First,Rest),
-    symmetric(Rest,[First]),
-    search(Assignments, 0, input_order, indomain, complete, [search_optimization(true)]),
-    results(AIds,Assignments,ASA),
-    makeASP(ASP,ASA,Sums).
+    first(Assignments, First, Rest),
+    symmetric(Rest, [First]),
 
-overlap([],[],[]).
-overlap([AId|RestAids],[Var|RestVars],[Var-Duration|RestDurations]):-
+    /* Search-Solutions */
+    search(Assignments, 0, input_order, indomain, complete, [search_optimization(true)]),
+    results(AIds, Assignments, ASA),
+    makeASP(ASP, ASA, Sums).
+first([X|Rest],X,Rest).
+
+overlap([], [], []).
+overlap([AId|RestAids], [Var|RestVars], [Var-Duration|RestDurations]) :-
     activity(AId, act(Ab, Ae)),
     Duration is Ae - Ab,
-    check(AId,RestAids,Var,RestVars),
-    overlap(RestAids,RestVars,RestDurations).
+    check(AId, RestAids, Var, RestVars),
+    overlap(RestAids, RestVars, RestDurations).
 
-check(_,[],_,[]).
-check(AId,[X|RestAids],Var,[XVar|RestVars]):-
+check(_, [], _, []).
+check(AId, [X|RestAids], Var, [XVar|RestVars]) :-
     activity(AId, act(Ab1, Ae1)),
     activity(X, act(Ab2, Ae2)),
     ((Ab1 > Ae2; Ae1 < Ab2) -> true;
     Var #\= XVar),
-    check(AId,RestAids,Var,RestVars).
+    check(AId, RestAids, Var, RestVars).
 
-results([],[],[]).
-results([AId|RestAids],[Var|RestVars],[AId-Var|RestASA]):-
-    results(RestAids,RestVars,RestASA).
+results([], [], []).
+results([AId|RestAids], [Var|RestVars], [AId-Var|RestASA]) :-
+    results(RestAids, RestVars, RestASA).
 
-makeASP([],_,[]).
-makeASP([N-AIds-Sum|Rest],ASA,[N-Sum|RestSums]):-
+makeASP([], _, []).
+makeASP([N-AIds-Sum|Rest], ASA, [N-Sum|RestSums]) :-
     findall(AId, (member(Element, ASA), match(N, Element, AId)), AIds),
-    makeASP(Rest,ASA,RestSums).
+    makeASP(Rest, ASA, RestSums).
 
 match(Worker, AId-Worker, AId).
 
 max_time(NP, _, NP, _, Sums, Sums).
-max_time(NP, Durations, N, MT, SoFar, Sums):-
+max_time(NP, Durations, N, MT, SoFar, Sums) :-
     NP > N,
     New is N + 1,
-    constraints(Durations,New,Sum),
+    constraints(Durations, New, Sum),
     Sum #< MT + 1,
-    append(SoFar,[New-Sum],S),
+    append(SoFar, [New-Sum], S),
     max_time(NP, Durations, New, MT, S, Sums).
 
-constraints([],_,0).
-constraints([Var-Duration|RestDurations],N,Sum):-
-    constraints(RestDurations,N,S),
+constraints([], _, 0).
+constraints([Var-Duration|RestDurations], N, Sum) :-
+    constraints(RestDurations, N, S),
     Sum #= S + (Var #= N)*Duration.
 
-symmetric([],_).
-symmetric([Var|RestVars],SoFar):-
+symmetric([], _).
+symmetric([Var|RestVars], SoFar) :-
     Var #=< max(SoFar) + 1,
-    append(SoFar,[Var],Max),
-    symmetric(RestVars,Max).
+    append(SoFar, [Var], Max),
+    symmetric(RestVars, Max).

60cb65d75d7d6682139f0225124e4884ac1e7938
sdi2000064@di.uoa.gr <sdi2000064@di.uoa.gr>
2023-06-26 13:39:41 +0300

    Refactor + Comments

--- Ask7/assignment_csp.pl
+++ Ask7/assignment_csp.pl
@@ -6,38 +6,39 @@
 
 assignment_csp(NP, MT, ASP, ASA) :-
     /* Data in Lists */
-    findall(AId, activity(AId, _), AIds),
-    length(AIds, N),
-    length(Assignments, N),
-    length(Durations, N),
+    findall(AId, activity(AId, _), AIds),  % Collect all activity IDs into a list
+    length(AIds, N),  % Get the total number of activities
+    length(Assignments, N),  % Create a list of assignment variables with the same length as activities
+    length(Durations, N),  % Create a list of duration variables with the same length as activities
 
     /* Constraints */
     Assignments #:: 1..NP,
-    overlap(AIds, Assignments, Durations),
-    max_time(NP, Durations, 0, MT, [], Sums),
+    overlap(AIds, Assignments, Durations),  % Apply the overlap constraint between activities and assignments
+    max_time(NP, Durations, 0, MT, [], Sums),  % Calculate the maximum time spent by each worker and collect the sums
     Assignments= [ First | Rest],
-    First #= 1,
-    symmetric(Rest, [First]),
+    First #= 1,  % The first assignment must be worker 1
+    symmetric(Rest, [First]),  % Enforce symmetry in the assignments
 
     /* Search-Solutions */
     search(Assignments, 0, input_order, indomain, complete, []),
-    results(AIds, Assignments, ASA),
-    makeASP(ASP, ASA, Sums).
+    results(AIds, Assignments, ASA),  % Collect the final assignment results
+    makeASP(ASP, ASA, Sums).  % Generate an answer set program based on the assignment results
 
 assignment_opt(NF, NP, MT, F, T, ASP, ASA, Cost):-
     
     /* Data in Lists */
-    findall(AId, activity(AId, _), AIdsAll),
-    length(AIdsAll,N1),
-    ( ( NF = 0; NF > N1 ) -> N = N1, AIds = AIdsAll;
-    length(AIds,NF),
+    findall(AId, activity(AId, _), AIdsAll),  % Collect all activity IDs into a list
+    length(AIdsAll, N1),  % Get the total number of activities
+    ( (NF = 0; NF > N1) -> N = N1, AIds = AIdsAll;  % If the number of fixed activities is 0 or greater than the total number of activities, use all activities
+    length(AIds, NF),  % Otherwise, use the specified number of fixed activities
     N = NF,
-    append(AIds, _, AIdsAll)),
+    append(AIds, _, AIdsAll)),  % Extract the first NF activities from the list
 
-    findall(Time, (member(AId, AIds), activity(AId, act(A,B)), Time is B-A), Durs),
-    sum_list(Durs,D),
-    length(Assignments, N),
-    length(Durations, N),
+    findall(Time, (member(AId, AIds), activity(AId, act(A,B)), Time is B - A), Durs),  % Calculate the durations of the selected activities
+    sum_list(Durs, D),  % Calculate the total duration of the selected activities
+    length(Assignments, N),  % Create a list of assignment variables with the same length as activities
+    length(Durations, N),  % Create a list of duration variables with the same length as activities
+
     /* Constraints */
     Assignments #:: 1..NP,
     overlap(AIds, Assignments, Durations),
@@ -47,9 +48,10 @@ assignment_opt(NF, NP, MT, F, T, ASP, ASA, Cost):-
     symmetric(Rest, [First]),
 
     /* Cost */
-    rnd(D, NP, A),
-    sum_cost(A, Sums, [], C),
-    Cost #= sum(C),
+    rnd(D, NP, A),  % Calculate the round division of D by NP
+    sum_cost(A, Sums, [], C),  % Calculate the cost based on the assignments and sums
+
+    Cost #= sum(C),  % The total cost is the sum of individual costs
     
     /* Search-Solutions */
     bb_min(search(Assignments, 0, most_constrained, indomain, complete, []), Cost,
@@ -59,18 +61,18 @@ assignment_opt(NF, NP, MT, F, T, ASP, ASA, Cost):-
     makeASP(ASP, ASA, Sums).
 
 overlap([], [], []).
-overlap([AId|RestAids], [Var|RestVars], [Var-Duration|RestDurations]) :-
-    activity(AId, act(Ab, Ae)),
-    Duration is Ae - Ab,
-    check(AId, RestAids, Var, RestVars),
+overlap([AId | RestAids], [Var | RestVars], [Var - Duration | RestDurations]) :-
+    activity(AId, act(Ab, Ae)),  % Get the start and end times of the activity
+    Duration is Ae - Ab,  % Calculate the duration of the activity
+    check(AId, RestAids, Var, RestVars),  % Check for overlap with other activities
     overlap(RestAids, RestVars, RestDurations).
 
 check(_, [], _, []).
 check(AId, [X|RestAids], Var, [XVar|RestVars]) :-
-    activity(AId, act(Ab1, Ae1)),
-    activity(X, act(Ab2, Ae2)),
-    (( Ae2 - Ab1 =< 1; Ab2-Ae1 >= 1) -> true;
-    Var #\= XVar),
+    activity(AId, act(Ab1, Ae1)),  % Get the start and end times of the current activity
+    activity(X, act(Ab2, Ae2)),  % Get the start and end times of the other activity
+    ((Ae2 - Ab1 =< 1; Ab2 - Ae1 >= 1) -> true;  % If the activities don't overlap in time, they are valid
+    Var #\= XVar),  % Otherwise, the assignments must be different
     check(AId, RestAids, Var, RestVars).
 
 max_time(NP, _, N, _, Sums, Sums):-
@@ -90,7 +92,7 @@ constraints([Var-Duration|RestDurations], N, F, S) :-
 
 symmetric([], _).
 symmetric([Var|RestVars], SoFar) :-
-    Var #=< max(SoFar) + 1,
+    Var #=< max(SoFar) + 1,  % The current assignment must be less than or equal to the maximum assignment so far plus 1
     append(SoFar, [Var], Miax),
     symmetric(RestVars, Miax).
 
